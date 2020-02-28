fabf2ee934c1bd01e86f9097ab8074da5d589575
Fankexin <974094944@qq.com>
2020-02-28 14:15:43 +0800

    .

--- rectangle.js
+++ rectangle.js
@@ -1,32 +1,26 @@
-/**
- * 小数点后面保留第 n 位
- *
- * @param x 做近似处理的数
- * @param n 小数点后第 n 位
- * @returns 近似处理后的数 
- */
+/* global $: true */
 function roundFractional(x, n) {
-    return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+  return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
 }
 
 $(function () {
-    // get dom elem
-    var $width = $('#width'),
-        $height = $('#height'),
-        $btnCal = $('#calculate'),
-        $perimeter = $('#perimeter'),
-        $area = $('#area');
-    /*calc button click event */
-    $btnCal.click(function () {
-        // get value
-        var w = Number($width.val()),
-            h = Number($height.val());
+  // get dom elem
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+  /*calc button click event */
+  $btnCal.click(function () {
+    // get value
+    var w = Number($width.val()),
+        h = Number($height.val());
         // calculate
-        var p = roundFractional(2 * (w + h),5) ,
-            a = roundFractional(w * h,5) ;
+    var p = roundFractional(2 * (w + h), 5),
+        a = roundFractional(w * h, 5);
         // output
-        $perimeter.val(p);
-        $area.val(a);
-    })
+    $perimeter.val(p);
+    $area.val(a);
+  });
 
 });
